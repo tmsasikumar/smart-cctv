6728cb1915e7ec95a13437ea70cf2b1bd4968284
Sasi Kumar T <sasi.kumar@reedexpo.co.uk>
2021-09-26 17:01:11 +0530

    Added syncthing script

--- README.md
+++ README.md
@@ -3,5 +3,9 @@
 
 pip3 install -r requirements.txt
 
+In ubuntu
+sudo apt-get install libjasper-dev
+sudo apt-get install libatlas-base-dev
+pip3 install numpy --upgrade
 
 python3 face_recognition.py
---
+++ install_syncthing.sh
@@ -0,0 +1,19 @@
+# For APT over HTTPS
+sudo apt-get install curl apt-transport-https
+
+# Add the release PGP keys:
+sudo curl -s -o /usr/share/keyrings/syncthing-archive-keyring.gpg https://syncthing.net/release-key.gpg
+
+# Add the "stable" channel to your APT sources:
+echo "deb [signed-by=/usr/share/keyrings/syncthing-archive-keyring.gpg] https://apt.syncthing.net/ syncthing stable" | sudo tee /etc/apt/sources.list.d/syncthing.list
+
+# Increase preference of Syncthing's packages ("pinning")
+printf "Package: *\nPin: origin apt.syncthing.net\nPin-Priority: 990\n" | sudo tee /etc/apt/preferences.d/syncthing
+
+# Update and install syncthing:
+sudo apt-get update
+sudo apt-get install syncthing
+
+# Enable Syncthing as system service
+# May need to replace "pi" with your username
+sudo systemctl enable syncthing@pi.service --now
